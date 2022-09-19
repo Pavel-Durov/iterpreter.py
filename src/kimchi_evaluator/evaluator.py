92c8aac2e19833c68099559c62fceb621d7a8f00
Pavel Durov <pav3ldurov@gmail.com>
2022-09-19 16:16:27 +0100

    chore: infix and prefix operations

--- src/kimchi_evaluator/evaluator.py
+++ src/kimchi_evaluator/evaluator.py
@@ -1,4 +1,4 @@
-from src.kimchi_ast.ast import ExpressionStatement, IntegerLiteral, Program, Boolean
+from src.kimchi_ast.ast import ExpressionStatement, InfixExpression, IntegerLiteral, PrefixExpression, Program, Boolean
 import src.kimchi_object.object as obj
 
 TRUE = obj.Boolean(True)
@@ -6,30 +6,93 @@ FALSE = obj.Boolean(False)
 NULL = obj.Null()
 
 def eval(node):
-  # statements
-  if isinstance(node, Program):
-    return eval_statements(node.statements)
+    # statements
+    if isinstance(node, Program):
+      return eval_statements(node.statements)
 
-  if isinstance(node, ExpressionStatement):
-    return eval(node.expression) 
+    if isinstance(node, ExpressionStatement):
+      return eval(node.expression) 
 
-  # expressions
-  if isinstance(node, IntegerLiteral):
-    return obj.Integer(node.value)
-  if isinstance(node, Boolean):
-    return native_bool_to_boolean_object(node.value)
-  
-  return None
+    # expressions
+    if isinstance(node, IntegerLiteral):
+      return obj.Integer(node.value)
+    elif isinstance(node, Boolean):
+      return native_bool_to_boolean_object(node.value)
+    elif isinstance(node, PrefixExpression):
+      right = eval(node.right)
+      return eval_prefix_expression(node.operator, right)
+    elif isinstance(node, InfixExpression):
+      left = eval(node.left)
+      right = eval(node.right)
+      return eval_infix_expression(node.operator, left, right)
+    return None
+
+def eval_integer_infix_expression(operator, left, right):
+    left_val = left.value
+    right_val = right.value
+    if operator == "+":
+      return obj.Integer(left_val + right_val)
+    elif operator == "-":
+      return obj.Integer(left_val - right_val)
+    elif operator == "*":
+      return obj.Integer(left_val * right_val)
+    elif operator == "/":
+      return obj.Integer(left_val / right_val)
+    elif operator == "<":
+      return native_bool_to_boolean_object(left_val < right_val)
+    elif operator == ">":
+      return native_bool_to_boolean_object(left_val > right_val)
+    elif operator == "==":
+      return native_bool_to_boolean_object(left_val == right_val)
+    elif operator == "!=":
+      return native_bool_to_boolean_object(left_val != right_val)
+    return NULL
+
+
+def eval_infix_expression(operator, left, right):
+    if isinstance(left, obj.Integer) and isinstance(right, obj.Integer):
+      return eval_integer_infix_expression(operator, left, right)
+    elif operator == "==":
+      return native_bool_to_boolean_object(left == right)
+    elif operator == "!=":
+      return native_bool_to_boolean_object(left != right)
+    return NULL
+
+
+def eval_bang_operator_expression(right):
+    if right == TRUE:
+      return FALSE
+    if right == FALSE:
+      return TRUE
+    if right == NULL:
+      return TRUE
+    return FALSE
+
+
+def eval_minus_prefix_operator_expression(right):
+    if isinstance(right, obj.Integer):
+      value = right.value
+      return obj.Integer(-value)
+    return NULL
+
+def eval_prefix_expression(operator, right):
+    if operator == "!":
+      return eval_bang_operator_expression(right)
+    elif operator == "-":
+      return eval_minus_prefix_operator_expression(right)
+    return NULL
+
 
 def eval_statements(statements):
-  result = None
+    result = None
 
-  for statement in statements:
-    result = eval(statement)
+    for statement in statements:
+      result = eval(statement)
 
-  return result
+    return result
+
 
 def native_bool_to_boolean_object(input):
-  if input:
-    return TRUE
-  return FALSE
+    if input:
+      return TRUE
+    return FALSE
--- src/kimchi_evaluator/test_evaluator.py
+++ src/kimchi_evaluator/test_evaluator.py
@@ -6,6 +6,19 @@ def test_eval_integer_expression():
     tests = [
       ("5", 5),
       ("10", 10),
+      ("-5", -5),
+      ("-10", -10),
+      ("5 + 5 + 5 + 5 - 10", 10),
+      ("2 * 2 * 2 * 2 * 2", 32),
+      ("-50 + 100 + -50", 0),
+      ("5 * 2 + 10", 20),
+      ("5 + 2 * 10", 25),
+      ("20 + 2 * -10", 0),
+      ("50 / 2 * 2 + 10", 60),
+      ("2 * (5 + 10)", 30),
+      ("3 * 3 * 3 + 10", 37),
+      ("3 * (3 * 3) + 10", 37),
+      ("(5 + 10 * 2 + 15 / 3) * 2 + -10", 50),
     ]
     for tt in tests:
       evaluated = eval_test(tt[0])
@@ -15,11 +28,42 @@ def test_boolean_expression():
     tests = [
       ("true", True),
       ("false", False),
+      ("1 < 2", True),
+      ("1 > 2", False),
+      ("1 < 1", False),
+      ("1 > 1", False),
+      ("1 == 1", True),
+      ("1 != 1", False),
+      ("1 == 2", False),
+      ("1 != 2", True),
+      ("true == true", True),
+      ("false == false", True),
+      ("true == false", False),
+      ("true != false", True),
+      ("false != true", True),
+      ("(1 < 2) == true", True),
+      ("(1 < 2) == false", False),
+      ("(1 > 2) == true", False),
+      ("(1 > 2) == false", True),
+
     ]
     for tt in tests:
       evaluated = eval_test(tt[0])
       assert_boolean_object(evaluated, tt[1])
  
+def test_bang_operator():
+    tests = [
+      ("!true", False),
+      ("!false", True),
+      ("!5", False),
+      ("!!true", True),
+      ("!!false", False),
+      ("!!5", True)
+    ]
+    for tt in tests:
+      evaluated = eval_test(tt[0])
+      assert_boolean_object(evaluated, tt[1])
+
 def eval_test(str):
     lexer = Lexer(str)
     parser = Parser(lexer)
